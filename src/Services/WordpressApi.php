43ad0ba108b54894ac1971c1dbcedc944bb0bad8
Simon Bennett <simon@simnom.co.uk>
2017-03-31 09:11:31 +0100

    Added posts query and start to think about pagination

--- src/Services/WordpressApi.php
+++ src/Services/WordpressApi.php
@@ -25,18 +25,26 @@ class WordpressApi
          return $this->_get('wp/v2/pages');
      }
 
+     public function posts($page=1)
+     {
+        return $this->get('wp/v2/posts', ['page' => $page]);
+     }
+
     /**
      * Process the required request and return a suitable json response
      * @param  string - $method - the api method to call
      * @return json - JSON response from the request
      */
-     public function _get($method)
+     public function _get($method, $params)
      {
          try {
              //Check if there's a valid cache entry
              return Cache::remember($method, $this->timeout, function() use ($method) {
+                 //Set some headers to make sure we get Json back
+                 $headers = ['Accept' => 'application/json'];
+
                  //If not send the request
-                 $response = $this->client->get($this->endpoint . $method);
+                 $response = $this->client->get($this->endpoint . $method, $headers, $params);
 
                  //check the response code
                  if ($response->code = 200) {
